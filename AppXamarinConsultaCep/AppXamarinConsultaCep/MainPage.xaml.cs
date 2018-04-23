9c4d4748126912c2341fb3f1365aeb9fe48aa827
jlopes <jean.llopes@outlook.com>
2018-04-23 10:25:32 -0300

    Add Query CEP

--- AppXamarinConsultaCep/AppXamarinConsultaCep/MainPage.xaml.cs
+++ AppXamarinConsultaCep/AppXamarinConsultaCep/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -13,5 +14,48 @@ namespace AppXamarinConsultaCep
 		{
 			InitializeComponent();
 		}
-	}
+
+
+        private async void BtnBuscarCep_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txtCep.Text))
+                {
+                    using (var client = new HttpClient())
+                    {
+                        using (var response = await client.GetAsync(string.Format("ssd{0}", txtCep.Text)))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var result = await response.Content.ReadAsStringAsync();
+
+                                if (!string.IsNullOrEmpty(result))
+                                {
+                                    await DisplayAlert("Parabéns", result, "Ok");
+                                }
+                                else
+                                {
+                                    throw new Exception("Não tivemos retorno da API");
+                                }
+                            }
+                            else
+                            {
+                                throw new Exception("Erro ao chamar a API");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    throw new InvalidOperationException("Erro ao informar o cep");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "Ok");
+            }
+        }
+
+    }
 }
