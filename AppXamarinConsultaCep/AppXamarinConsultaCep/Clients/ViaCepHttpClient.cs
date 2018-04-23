be38fc6375ad2548741dc29f1fbb7addc6be2af4
jlopes <jean.llopes@outlook.com>
2018-04-23 11:12:33 -0300

    Add Pattern

---
+++ AppXamarinConsultaCep/AppXamarinConsultaCep/Clients/ViaCepHttpClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppXamarinConsultaCep.Clients
+{
+    class ViaCepHttpClient
+    {
+        private static Lazy<ViaCepHttpClient> _lazy = new Lazy<ViaCepHttpClient>(() => new ViaCepHttpClient());
+        private readonly HttpClient _httpClient;
+
+        public static ViaCepHttpClient Current { get => _lazy.Value; }
+
+        private ViaCepHttpClient()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> BuscarCep(string cep)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(cep))
+                {
+                    using (var client = new HttpClient())
+                    {
+                        using (var response = await _httpClient.GetAsync(string.Format("https://viacep.com.br/ws/{0}/json/", cep)))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                return await response.Content.ReadAsStringAsync();
+                            }
+                            else
+                            {
+                                throw new InvalidOperationException("Erro ao chamar a API");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    throw new InvalidOperationException("Erro ao informar o cep");
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+        }
+    }
+}
--- AppXamarinConsultaCep/AppXamarinConsultaCep/MainPage.xaml.cs
+++ AppXamarinConsultaCep/AppXamarinConsultaCep/MainPage.xaml.cs
@@ -14,44 +14,8 @@ namespace AppXamarinConsultaCep
 
 		protected async void BtnBuscarCep_Clicked(object sender, EventArgs e)
 		{
-			try
-			{
-				if (!string.IsNullOrEmpty(txtCep.Text))
-				{
-					using (var client = new HttpClient())
-					{
-						using (var response = await client.GetAsync(string.Format("https://viacep.com.br/ws/{0}/json/", txtCep.Text)))
-						{
-							if (response.IsSuccessStatusCode)
-							{
-								var result = await response.Content.ReadAsStringAsync();
-
-								if (!string.IsNullOrEmpty(result))
-								{
-									await DisplayAlert("Parabéns", result, "Ok");
-								}
-								else
-								{
-									throw new InvalidOperationException("Não tivemos retorno da API");
-								}
-							}
-							else
-							{
-								throw new InvalidOperationException("Erro ao chamar a API");
-							}
-						}
-					}
-				}
-				else
-				{
-					throw new InvalidOperationException("Erro ao informar o cep");
-				}
-			}
-			catch (Exception ex)
-			{
-				await DisplayAlert("Erro", ex.Message, "Ok");
-			}
-		}
+            await DisplayAlert("Parabéns", result, "Ok");
+        }
 
 	}
 }
